f5948e656a01f66ea7ef6b404dc3b7c87ce062c2
GangolfOvaert <mail@choechsner.de>
2019-05-16 10:35:37 +0200

    Added first Page

---
+++ index_temp.php
@@ -0,0 +1,4 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="stylesheet.css" media="screen" />
+</head>
---
+++ login.php
@@ -0,0 +1,5 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="res/css/page_login.css" media="screen" />
+</head>
+<div class="login_background">
