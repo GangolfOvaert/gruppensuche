942429e25b96d5f1132ea9733060ca8588a007a9
GangolfOvaert <mail@choechsner.de>
2019-05-16 10:53:21 +0200

    added style css

--- login.php
+++ login.php
@@ -1,5 +1,8 @@
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="res/css/style.css" media="screen" />
 <link rel="stylesheet" type="text/css" href="res/css/page_login.css" media="screen" />
 </head>
-<div class="login_background">
+<div class="main_title">
+  Login
+</div>
